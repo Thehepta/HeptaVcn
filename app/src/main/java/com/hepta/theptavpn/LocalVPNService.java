464b90a5d81ec0756716fc83a905baa079f7d605
chicwalkiog <rizixi@foxmail.com>
2023-02-18 23:42:24 +0800

    first commit

---
+++ app/src/main/java/com/hepta/theptavpn/LocalVPNService.java
@@ -0,0 +1,265 @@
+package com.hepta.theptavpn;
+
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.ParcelFileDescriptor;
+
+import android.util.Log;
+
+import org.apache.commons.io.HexDump;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import java.io.Closeable;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Selector;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class LocalVPNService extends VpnService
+{
+    public static final String ACTION_DISCONNECT = "ACTION_DISCONNECT";
+    private static final String TAG = LocalVPNService.class.getSimpleName();
+    private static final String VPN_ADDRESS = "10.120.0.1"; // Only IPv4 support for now
+    private static final String VPN_ROUTE = "0.0.0.0"; // Intercept everything
+
+
+    private static boolean isRunning = false;
+
+    private ParcelFileDescriptor vpnInterface = null;
+
+    private PendingIntent pendingIntent;
+
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+    private ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue;
+    private ExecutorService executorService;
+
+    private Selector udpSelector;
+    private Selector tcpSelector;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    private void setupVPN()
+    {
+        if (vpnInterface == null)
+        {
+            Builder builder = new Builder();
+            builder.addAddress(VPN_ADDRESS, 32);
+            builder.addRoute(VPN_ROUTE, 0);
+            vpnInterface = builder.setSession(getString(R.string.app_name)).setConfigureIntent(pendingIntent).establish();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null && ACTION_DISCONNECT.equals(intent.getAction())) {
+            disconnect();
+            return START_NOT_STICKY;
+        } else {
+            connect();
+            return START_STICKY;
+        }
+    }
+
+    @Override
+    public boolean stopService(Intent name) {
+        return super.stopService(name);
+    }
+
+    private void disconnect() {
+        isRunning = false;
+        executorService.shutdownNow();
+        cleanup();
+        Log.i(TAG, "Stopped");
+    }
+
+    private void connect() {
+        isRunning = true;
+        setupVPN();
+        try
+        {
+            udpSelector = Selector.open();
+            tcpSelector = Selector.open();
+            deviceToNetworkUDPQueue = new ConcurrentLinkedQueue<>();
+            deviceToNetworkTCPQueue = new ConcurrentLinkedQueue<>();
+            networkToDeviceQueue = new ConcurrentLinkedQueue<>();
+
+            executorService = Executors.newFixedThreadPool(5);
+            executorService.submit(new UDPInput(networkToDeviceQueue, udpSelector));
+            executorService.submit(new UDPOutput(deviceToNetworkUDPQueue, udpSelector, this));
+            executorService.submit(new TCPInput(networkToDeviceQueue, tcpSelector));
+            executorService.submit(new TCPOutput(deviceToNetworkTCPQueue, networkToDeviceQueue, tcpSelector, this));
+            executorService.submit(new VPNRunnable(vpnInterface.getFileDescriptor(),
+                    deviceToNetworkUDPQueue, deviceToNetworkTCPQueue, networkToDeviceQueue));
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(BROADCAST_VPN_STATE).putExtra("running", true));
+            Log.i(TAG, "Started");
+        }
+        catch (IOException e)
+        {
+            // TODO: Here and elsewhere, we should explicitly notify the user of any errors
+            // and suggest that they stop the service, since we can't do it ourselves
+            Log.e(TAG, "Error starting service", e);
+            cleanup();
+        }
+    }
+
+    public static boolean isRunning()
+    {
+        return isRunning;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+    }
+
+    private void cleanup()
+    {
+        deviceToNetworkTCPQueue = null;
+        deviceToNetworkUDPQueue = null;
+        networkToDeviceQueue = null;
+        ByteBufferPool.clear();
+        closeResources(udpSelector, tcpSelector, vpnInterface);
+        vpnInterface = null;
+    }
+
+    // TODO: Move this to a "utils" class for reuse
+    private static void closeResources(Closeable... resources)
+    {
+        for (Closeable resource : resources)
+        {
+            try
+            {
+                resource.close();
+            }
+            catch (IOException e)
+            {
+                // Ignore
+            }
+        }
+    }
+
+    private static class VPNRunnable implements Runnable
+    {
+        private static final String TAG = VPNRunnable.class.getSimpleName();
+
+        private FileDescriptor vpnFileDescriptor;
+
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue;
+        private ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue;
+        private ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue;
+
+        public VPNRunnable(FileDescriptor vpnFileDescriptor,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkUDPQueue,
+                           ConcurrentLinkedQueue<Packet> deviceToNetworkTCPQueue,
+                           ConcurrentLinkedQueue<ByteBuffer> networkToDeviceQueue)
+        {
+            this.vpnFileDescriptor = vpnFileDescriptor;
+            this.deviceToNetworkUDPQueue = deviceToNetworkUDPQueue;
+            this.deviceToNetworkTCPQueue = deviceToNetworkTCPQueue;
+            this.networkToDeviceQueue = networkToDeviceQueue;
+        }
+
+        @Override
+        public void run()
+        {
+            Log.i(TAG, "Started");
+
+            FileChannel vpnInput = new FileInputStream(vpnFileDescriptor).getChannel();
+            FileChannel vpnOutput = new FileOutputStream(vpnFileDescriptor).getChannel();
+
+            try
+            {
+//                ByteBufUtil.
+                ByteBuffer bufferToNetwork = null;
+                boolean dataSent = true;
+                boolean dataReceived;
+                while (!Thread.interrupted())
+                {
+                    if (dataSent)
+                        bufferToNetwork = ByteBufferPool.acquire();
+                    else
+                        bufferToNetwork.clear();
+
+                    // TODO: Block when not connected
+                    int readBytes = vpnInput.read(bufferToNetwork);
+                    if (readBytes > 0)
+                    {
+                        dataSent = true;
+                        bufferToNetwork.flip();
+                        OutputStream os = new ByteArrayOutputStream();
+                        HexDump.dump(bufferToNetwork.array(), 0, os, 0);
+                        Log.e("rzx",  os.toString());
+                        Packet packet = new Packet(bufferToNetwork);
+                        if (packet.isUDP())
+                        {
+                            deviceToNetworkUDPQueue.offer(packet);
+                        }
+                        else if (packet.isTCP())
+                        {
+                            deviceToNetworkTCPQueue.offer(packet);
+                        }
+                        else
+                        {
+                            Log.w(TAG, "Unknown packet type");
+//                            Log.w(TAG, packet.ip4Header.toString());
+                            dataSent = false;
+                        }
+                    }
+                    else
+                    {
+                        dataSent = false;
+                    }
+
+                    ByteBuffer bufferFromNetwork = networkToDeviceQueue.poll();
+                    if (bufferFromNetwork != null)
+                    {
+                        bufferFromNetwork.flip();
+                        while (bufferFromNetwork.hasRemaining())
+                            vpnOutput.write(bufferFromNetwork);
+                        dataReceived = true;
+
+                        ByteBufferPool.release(bufferFromNetwork);
+                    }
+                    else
+                    {
+                        dataReceived = false;
+                    }
+
+                    // TODO: Sleep-looping is not very battery-friendly, consider blocking instead
+                    // Confirm if throughput with ConcurrentQueue is really higher compared to BlockingQueue
+                    if (!dataSent && !dataReceived)
+                        Thread.sleep(10);
+                }
+            }
+            catch (InterruptedException e)
+            {
+                Log.i(TAG, "Stopping");
+            }
+            catch (IOException e)
+            {
+                Log.w(TAG, e.toString(), e);
+            }
+            finally
+            {
+                closeResources(vpnInput, vpnOutput);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/hepta/theptavpn/MainActivity.java
@@ -0,0 +1,89 @@
+package com.hepta.theptavpn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.hepta.theptavpn.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'theptavpn' library on application startup.
+    static {
+        System.loadLibrary("theptavpn");
+    }
+    private static final int VPN_REQUEST_CODE = 0x0F;
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+        binding.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVPN();
+            }
+        });
+        binding.stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("Rzx","stop");
+                startService(getServiceIntent().setAction(LocalVPNService.ACTION_DISCONNECT));
+            }
+        });
+
+
+    }
+
+
+    private void startVPN()
+    {
+        Intent vpnIntent = VpnService.prepare(this);
+        if (vpnIntent != null)
+            startActivityForResult(vpnIntent, VPN_REQUEST_CODE);
+        else
+            onActivityResult(VPN_REQUEST_CODE, RESULT_OK, null);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            startService(getServiceIntent());
+        }
+    }
+
+    @Override
+    public boolean stopService(Intent name) {
+        return super.stopService(name);
+    }
+
+    private Intent getServiceIntent() {
+
+        return new Intent(getApplicationContext(), LocalVPNService.class);
+
+    }
+
+    /**
+     * A native method that is implemented by the 'theptavpn' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
