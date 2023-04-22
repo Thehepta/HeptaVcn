75f593623baa773c5d6f0c9647cd84dbd9e2f8ff
rzx <rizixi@foxmail.com>
2023-04-22 21:08:01 +0800

    fix file

--- libevent/build/intermediates/cxx/Debug/c16g1j1h/logs/arm64-v8a/build_command_libevent.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\thehepta\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\git\\theptavpn\\libevent\\.cxx\\Debug\\c16g1j1h\\arm64-v8a" ^
-  libevent
